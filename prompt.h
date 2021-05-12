2fd55b064b1eee730c6f76bddfd63ea180446f5f
Firmin Martin <firminmartin24@gmail.com>
2021-05-12 13:06:02 +0200

    prompt.h: clarify the non-use of git_prompt
    It's really tempting to use git_prompt to prompt user for input, but in
    most of the case, we must not [1]. Reflect this as a comment in prompt.h.
    
    [1]: https://lore.kernel.org/git/YJTH+sTP%2FO5Nxtp9@coredump.intra.peff.net/
    
    Reported-by: Jeff King <peff@peff.net>
    Signed-off-by: Firmin Martin <firminmartin24@gmail.com>

--- prompt.h
+++ prompt.h
@@ -4,6 +4,16 @@
 #define PROMPT_ASKPASS (1<<0)
 #define PROMPT_ECHO    (1<<1)
 
+/*
+ * This function should not be used for regular prompts (i.e., asking user for
+ * confirmation or picking an option from an interactive menu) as it only
+ * accepts input from /dev/tty, thus making it impossible to test with the
+ * current test suite.  Please instead use git_read_line_interactively for that
+ * purpose.  See 97387c8bdd (am: read interactive input from stdin, 2019-05-20)
+ * for historical context.
+ *
+ */
+
 char *git_prompt(const char *prompt, int flags);
 
 int git_read_line_interactively(struct strbuf *line);
